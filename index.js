e75818827f149e366218f4f11b997a250ae64fe1
devcem <commention@gmail.com>
2017-07-06 21:36:08 +0300

    Encode function with returning hex

--- index.js
+++ index.js
@@ -8,10 +8,10 @@
 */
 const pack = {
 	encode : function(array){
-		return this.arrayToBinary(array).length;
+		return this.arrayToBinary(array);
 	},
 	decode : function(string){
-		return string;
+		return this.binaryToArray();
 	},
 	detect : function(data){
 		if(!isNaN(parseFloat(data)) && isFinite(data)){
@@ -43,26 +43,23 @@ const pack = {
 	binary : function(length, array){
 		var header = pack.header(array);
 		var length = length;
-		var writer = new Buffer(length);
-		var count  = 0;
-
-		console.log(array);
+		var writer = pack.num2hex(header.length/2) + pack.bin2hex(header);
 
 		for(item in array){
 			item = array[item];
 
 			switch(item.type) {
+				case 1:
+			        writer+=item.byte.toString();
+			        break;
 			    case 8:
-			        writer.writeUInt8(item.byte, count);
-					count+=item.type/8;
+			        writer+=pack.writeUInt8Buffer(item.byte);
 			        break;
 			    case 32:
-			        writer.writeInt32LE(item.byte, count);
-					count+=item.type/8;
+			        writer+=pack.writeInt32Buffer(item.byte);
 			        break;
 			    case 64:
-			        writer.writeFloatLE(item.byte, count);
-					count+=4;
+			        writer+=pack.writeFloatBuffer(item.byte);
 			        break;
 			}
 		}
@@ -80,7 +77,43 @@ const pack = {
 
 	isFloat : function(n){
 	    return Number(n) === n && n % 1 !== 0;
+	},
+	writeUInt8Buffer : function(num) {
+		const buf = new Buffer(1);
+		buf.writeUInt8(num, 0);
+		return buf.toString('hex');
+	},
+	writeInt32Buffer : function(num) {
+		const buf = new Buffer(4);
+		buf.writeInt32LE(num, 0);
+		return buf.toString('hex');
+	},
+	writeFloatBuffer : function(num) {
+		const buf = new Buffer(4);
+		buf.writeFloatLE(num, 0);
+		return buf.toString('hex');
+	},
+	pad : function(n, width, z){
+		z = z || '0';
+	  	n = n + '';
+	  	return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+	},
+
+	hex2bin : function(string){
+		return string.match(/.{1,2}/g).map(function(part){
+			return pack.pad((parseInt(part, 16)).toString(2),8);
+		}).join('');
+	},
+
+	bin2hex : function(string){
+		return string.match(/.{1,8}/g).map(function(part){
+			return pack.pad((parseInt(part, 2)).toString(16), 2);
+		}).join('');
+	},
+
+	num2hex : function(number){
+		return pack.pad((number).toString(16), 2);
 	}
 };
 
-console.log(pack.encode([1000, 1233, 3233, 33, 3.1, 23, 1,2,3,4,5, 100, 12, 322, 312]));
+console.log(pack.encode([0,4,0.2,0.3,16,1234,256,223,300,1.3,0,1,100,0.3,1,300,23,0.1,0.3,0.1,1,2,3,4,5,1.055,0.2]));
